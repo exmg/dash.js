49a669a9ec31ef3d216df2b53befd9edffb27d2d
Stephan Hesse <tchakabam@gmail.com>
2020-06-10 13:31:34 +0200

    exmg: fix pub-locking in key-post tool

--- exmg-key-pub-http.js
+++ exmg-key-pub-http.js
@@ -14,20 +14,40 @@ const remoteHost = `p-ep${remoteStreamId}.i.akamaientrypoint.net`;
 const remoteBasePath = `/cmaf/live/${remoteStreamId}/${remoteEventName}/`;
 
 const dirPath = path.resolve(process.argv[2]);
+if (!dirPath) {
+    throw new Error('Need to specify a key-file source directory');
+}
+
+const subFolder = process.argv[3];
+if (!subFolder) {
+    throw new Error('Need to specify a sub-folder parameter for remote path construction')
+}
 
 const JOB_POLL_MS = 1000;
 
+const PUB_LOCK_FILE_EXT = '.lock';
+
 function doHttpPost(hostname, path, data, mimeType) {
-    return phin({
-        url: 'https://' + hostname + path,
+
+    const url =  'http://' + hostname + path;
+    const phinOpts = {
+        url,
         method: 'POST',
-        data,
+        data: data.toString('utf8'), // Q
         core: {
             headers: {
-                'Content-Type': mimeType
+                'Connection': 'keep-alive',
+                'Accept': '*/*',
+                'User-Agent': 'Akamai_Broadcaster_v1.0',
+                //'Icy-MetaData': '1',
+                //'Content-Type': mimeType
             }
         }
-    })
+    }
+
+    console.log('Doing HTTP-request:', phinOpts);
+
+    return phin(phinOpts)
 }
 
 function getDirFilesListing(dir) {
@@ -57,37 +77,56 @@ function readFile(path) {
 
 function writeLockFile(path) {
     return new Promise((res, rej) => {
-        fs.writeFile(path + '.lock', null, res);
+        fs.writeFile(path + PUB_LOCK_FILE_EXT, null, res);
     });
 }
 
 function scanAndPublish() {
     let filesList;
+
     return getDirFilesListing(dirPath).then((files) => {
 
-        files = files.filter((f) => ! f.endsWith('.lock'));
+        console.log('Scanned files in dir:', files)
 
-        filesList = files;
-        console.log('Scanned files:', files)
-        return Promise.all(files.map((filename) => {
+        // Exclude all files that have a PUB_LOCK_FILE_EXT
+        // and add the prequel to the current lock-set,
+        // then filter all actual non-lock files through the lock-set.
+        // (coherent within one scan-run)
+        const lockSet = new Set();
+        filesList = files = files.filter((f) => {
+            if(f.endsWith(PUB_LOCK_FILE_EXT)) {
+                lockSet.add(f.substr(0, f.length - 5));
+                return false;
+            }
+            return true;
+        }).filter((f) => !lockSet.has(f));
+
+        console.log('Non pub-lock`d files found:', filesList);
+
+        return Promise.all(files.map((filename, index) => {
             console.log('reading file:', filename)
             return readFile(path.join(dirPath, filename))
-        }))
+        }));
+
     }).then((bufferList) => {
         console.log('Read buffers:', bufferList.length)
         return Promise.all(bufferList.map((buf, index) => {
+
             const filename = filesList[index];
-            const remoteFilePath = remoteBasePath + filename;
-            //const mimeType = filename.endsWith('.json') ? 'application/json' : 'text/plain';
+            const remoteFilePath = remoteBasePath + subFolder + '/' + filename;
 
-            const mimeType = 'video/mp4';
+            const mimeType = filename.endsWith('.json') ? 'application/json' : 'text/plain';
 
-            console.log('Doing POST request:', filename, remoteHost, remoteFilePath, mimeType)
+            //const mimeType = 'video/mp4';
 
             return doHttpPost(remoteHost, remoteFilePath, buf, mimeType)
                 .then((res) => {
-                    console.log('Done POST:', remoteFilePath)
+                    console.log('Done POST:', res.url)
                     console.log('Server response:', res.body.toString('utf8'))
+
+                    if (res.statusCode >= 400) {
+                        throw new Error(`HTTP-response status: ${res.statusMessage} (${res.statusCode}) (${remoteFilePath})`);
+                    }
                 })
                 .then(() => writeLockFile(path.resolve(dirPath, filename)))
         }))
