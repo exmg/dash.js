ec316bd2fb75fca3d57f5d70d398eecd715426d7
Stephan Hesse <tchakabam@gmail.com>
2020-06-10 15:35:50 +0200

    key-pub server finalization for being operational

--- exmg-key-pub-srv.js
+++ exmg-key-pub-srv.js
@@ -1,11 +1,58 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mkdirp = require('mkdirp')
 const app = express();
+const port = 3002;
 
-app.use(express.json());
+const PUB_DIR = 'public';
 
-app.post('/*', function(request, response){
-    console.log(request.body);      // your JSON
-    response.send(request.body);    // echo the result back
+// @see https://github.com/expressjs/cors#configuring-cors
+app.use(cors(/* specific config here */)); // <--- CORS-requests support
+// enable pre-flight across-the-board
+app.options("*", cors()); // @see https://github.com/expressjs/cors#enabling-cors-pre-flight
+
+//app.use(express.json({type: 'application/json'}));
+app.use(bodyParser.text({type: 'text/plain'}));
+app.use(bodyParser.text({type: 'application/json'}));
+
+app.use(express.static(PUB_DIR));
+
+app.post('/*', function(request, response) {
+
+    console.log(request.path)
+    console.log(request.body);
+
+    const data = request.body;
+
+    const fileOutPath = path.resolve(PUB_DIR + request.path);
+
+    mkdirp(path.dirname(fileOutPath)).then(() => {
+
+        fs.writeFile(fileOutPath, data, {flag: 'w'}, (err) => {
+
+            if (err) {
+                console.error('Failure writing file:', err.message);
+                response.writeHead(500, err.message);
+                response.write('ERROR: ' + err.message);
+                response.end();
+                return;
+            }
+
+            response.writeHead(200);
+            response.write('OK');
+            response.end();
+
+        });
+
+    });
+
 });
 
-app.listen(3001);
+app.listen(port, () => {
+    console.log('Ready on port:', port);
+});
